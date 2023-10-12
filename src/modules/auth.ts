66a6d3c29192183cf4ad0bd8e79b9389e338a810
Shenon69 <ask.tikki69@gmail.com>
2023-10-12 20:36:23 +0530

    feat: middleware with jwt

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -10,3 +10,32 @@ export const createJWT = (user) => {
 
     return token
 }
+
+export const protect = (req,res,next) => {
+    const bearer = req.headers.authorization;
+
+    if(!bearer){
+        res.status(401)
+        res.json({message:"Not authorized!"})
+        return
+    }
+
+    const [,token] = bearer.split(" ");
+
+    if(!token){
+        res.status(401)
+        res.json({message:"Not a vaild token!"})
+        return
+    }
+
+    try {
+       const user = jwt.verify(token,process.env.JWT_SECRET) 
+       req.user = user;
+       next()
+    } catch (error) {
+        console.log(error)
+        res.status(401)
+        res.json({message:"Not authorized!"})
+        return
+    }
+}
